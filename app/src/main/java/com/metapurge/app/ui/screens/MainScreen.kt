f6a67fb7b60285e6dc9f666867a1811d57f8c82d
potatameister <180633607+potatameister@users.noreply.github.com>
2026-03-01 23:34:59 +0700

    Fix missing border import

--- app/src/main/java/com/metapurge/app/ui/screens/MainScreen.kt
+++ app/src/main/java/com/metapurge/app/ui/screens/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
