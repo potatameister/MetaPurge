9f0ca72b8901e3d1042fe40d54236c00067cd63f
potatameister <180633607+potatameister@users.noreply.github.com>
2026-03-01 16:45:38 +0700

    Update Kotlin to 2.0.21 for Gradle 9 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
     id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
