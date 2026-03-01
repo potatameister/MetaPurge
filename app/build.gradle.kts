120408585e31f0d30ec8a6f46c7b132b59214bfd
potatameister <180633607+potatameister@users.noreply.github.com>
2026-03-01 16:16:55 +0700

    Update to Gradle 9.3.1 and AGP 9.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,16 +55,6 @@ android {
     }
 }
 
-android.applicationVariants.all { variant ->
-    variant.outputs.forEach { output ->
-        val outputFile = output.asFile.get()
-        if (outputFile.extension == "apk") {
-            val newFile = outputFile.parentFile?.resolve("MetaPurge.apk")
-            output.outputFileName = "MetaPurge.apk"
-        }
-    }
-}
-
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
